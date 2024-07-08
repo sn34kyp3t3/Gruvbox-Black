9f6f68bf4ec9ab9e202cc7eff6ba383d60d2a72a
GitHub Action <action@github.com>
2024-07-08 12:16:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/sn34kyp3t3/gruvboxblack/MyBundle.kt
+++ src/main/kotlin/com/github/sn34kyp3t3/gruvboxblack/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sn34kyp3t3.gruvboxblack
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sn34kyp3t3/gruvboxblack/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/sn34kyp3t3/gruvboxblack/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.sn34kyp3t3.gruvboxblack.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
